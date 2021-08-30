c6d3c76395f968f94fecf2d4b89d06413de01683
gargVader <girishgargcool@gmail.com>
2021-08-30 10:15:05 +0530

    added outsideTargetAndMessage dismissType

--- showcaseviewlib/src/main/java/smartdevelop/ir/eram/showcaseviewlib/config/DismissType.java
+++ showcaseviewlib/src/main/java/smartdevelop/ir/eram/showcaseviewlib/config/DismissType.java
@@ -4,5 +4,5 @@ package smartdevelop.ir.eram.showcaseviewlib.config;
  * Created by Mohammad Reza Eram (https://github.com/mreram) on 27,November,2018
  */
 public enum DismissType {
-    outside, anywhere, targetView, selfView
+    outside, anywhere, targetView, selfView, outsideTargetAndMessage
 }
