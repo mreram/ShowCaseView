2fddc95dcb0c27275437835a330a96d139467f24
Erenis Ramadani <erenisramadani@gmail.com>
2024-01-26 13:32:50 +0100

    Remove original library files, as migrated to kotlin

--- showcaseviewlib/src/main/java/smartdevelop/ir/eram/showcaseviewlib/config/DismissType.java
+++
@@ -1,8 +0,0 @@
-package smartdevelop.ir.eram.showcaseviewlib.config;
-
-/**
- * Created by Mohammad Reza Eram (https://github.com/mreram) on 27,November,2018
- */
-public enum DismissType {
-    outside, anywhere, targetView, selfView, outsideTargetAndMessage
-}
--- showcaseviewlib/src/main/java/smartdevelop/ir/eram/showcaseviewlib/listener/GuideListener.java
+++
@@ -1,7 +0,0 @@
-package smartdevelop.ir.eram.showcaseviewlib.listener;
-
-import android.view.View;
-
-public interface GuideListener {
-    void onDismiss(View view);
-}
